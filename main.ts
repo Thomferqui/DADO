89b20e896870313e2612fb4ca3968112ece32e66
Thomferqui <73257143+Thomferqui@users.noreply.github.com>
2020-11-05 10:34:18 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+input.onButtonPressed(Button.A, function () {
+    DADO = randint(1, 6)
+    if (DADO == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (DADO == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (DADO == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (DADO == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (DADO == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+let DADO = 0
+basic.showString("HOLA MI NOMBRE ES THOMAS ANDRES FERNANDEZ QUINTERO DE 705")
